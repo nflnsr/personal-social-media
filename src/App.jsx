613f0815242100fe7f3dabf4978cb20e47500fec
youremail@yourdomain.com <naufaln176@gmail.com>
2023-03-12 04:47:10 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ import sunIcon from "./assets/sunIcon.svg";
 function App() {
   console.log("test");
   return (
-    <div className="bg-black h-screen">
-      <div className="container w-max m-auto">
+    <div className="bg-black h-screen w-screen">
+      <div className="w-max m-auto">
         <DeviceFrameset
           device="iPhone X"
           color="gold"
