39d37e5cb78c499d06b72b2077c6ff344bccfcda
youremail@yourdomain.com <naufaln176@gmail.com>
2023-03-12 07:32:24 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Notif from "./components/notifications";
 import sunIcon from "./assets/sunIcon.svg";
 
 function App() {
-  console.log("test");
+  if (window.innerWidth > 768) {
   return (
     <div className="bg-black h-screen w-screen">
       <div className="w-max m-auto lg:block hidden">
@@ -19,7 +19,7 @@ function App() {
           color="gold"
           potrait
           height={window.innerHeight - 80}
-          width={window.innerWidth - 80}
+          
         >
           <Notif />
           <div className="float-right px-2 py-2">
@@ -59,7 +59,51 @@ function App() {
         </DeviceFrameset>
       </div>
     </div>
-  );
+  );} else {
+    return (
+      <div className="bg-black h-screen w-screen">
+      <div className="w-max m-auto lg:block hidden">
+        
+          <Notif />
+          <div className="float-right px-2 py-2">
+            <img src={menuIcon} alt="" />
+          </div>
+          <div className="h-full py-12 bg-gradient-to-b from-lime-200 via-cyan-300 to-cyan-200">
+            <div className="w-fit mx-auto">
+              <img src={sunIcon} alt="" />
+            </div>
+            <div className="w-fit m-auto py-2 ">
+              {/* <div className="bg-black rounded-full fixed opacity-20 w-28 z-10 h-28">
+          
+              </div> */}
+              <a href="https://reactjs.org" target="_blank">
+                <img
+                  src={photoProfile}
+                  className="logo react hover:drop-shadow-2xl hover:shadow-none rounded-full w-28 bg-opacity-20 bg-black shadow-inner shadow-gray-500 z-20"
+                  alt="React logo"
+                />
+              </a>
+            </div>
+            <div className="py-4">
+              <div className="w-fit m-auto">
+                <SocmedCont />
+                <SocmedCont />
+                <SocmedCont />
+                <SocmedCont />
+                <SocmedCont />
+              </div>
+            </div>
+          <footer className="">
+            <div className="w-fit m-auto">
+              Copyright © 2023 Naufal Nasrullah ∙ All Rights Reserved
+            </div>
+          </footer>
+          </div>
+       
+      </div>
+    </div>
+    );
+}
 }
 
 export default App;
